47a6a355ac9ce5c599ee49652e44f177dce09dc1
dianlujitao <dianlujitao@lineageos.org>
2019-01-31 22:26:17 +0800

    lithium: Use libcryptfs_hw from commonsys repo
    Change-Id: I342f57e9e66cd4f6674d7ce318a2a8dabebef8c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ TARGET_BOARD_PLATFORM := msm8996
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno530
 
 # Encryption
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Partitions
